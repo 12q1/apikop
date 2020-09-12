bfbac64ffdcc2e0f23f2c1d914499b6bd6e6da1f
12q1 <thomasham89@gmail.com>
2020-09-12 06:18:47 +0200

    Updates

---
+++ precache-manifest.824aa3164accc5bc9586728ee9f39720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a3718b0d664891a1fed6d47a522266",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0803e5e2df26f8216bfc",
+    "url": "/static/css/main.22161708.chunk.css"
+  },
+  {
+    "revision": "4fc13321587a264ae6ad",
+    "url": "/static/js/2.f9304f2e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.f9304f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0803e5e2df26f8216bfc",
+    "url": "/static/js/main.3df03bf2.chunk.js"
+  },
+  {
+    "revision": "63d50598c2190200eea2",
+    "url": "/static/js/runtime-main.19fc50cd.js"
+  },
+  {
+    "revision": "0996b000078dfb7fc0f67e1aab4d306a",
+    "url": "/static/media/lowpolyape.0996b000.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/apikop/precache-manifest.e46855b8a7395aa2d28c26ae00aaa837.js"
+  "/precache-manifest.824aa3164accc5bc9586728ee9f39720.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/apikop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
